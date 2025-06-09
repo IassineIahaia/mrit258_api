197bd4103c9a561b3d53484a813539e0aae2d149
Iassine Iahaia <zinhocabila58@gmail.com>
2025-06-09 21:30:37 +0200

    Listando as categories

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -1,18 +1,34 @@
+const Category = require('../models/Category');
+
 module.exports = {
-
     getCategories: async (req, res) => {
+        try {
+            let cats = await Category.find();
+         
+            let categories = [];
+            for (let cat of cats) {
+                categories.push({
+                    ...cat.toObject(),
+                    img: `${process.env.BASE}/assets/images/${cat.slug}.png`
+                });
+            }
 
+            res.json({ categories });
+
+        } catch (err) {
+            res.status(500).json({ error: 'Server error', details: err.message });
+        }
     },
+
     editAction: async (req, res) => {
-
+        // Implementar a lógica aqui
     },
+
     getList: async (req, res) => {
-
+        // Implementar a lógica aqui
     },
+
     getItem: async (req, res) => {
-
-    },
-    editAction: async (req, res) => {
-
+        // Implementar a lógica aqui
     }
-}
+};
